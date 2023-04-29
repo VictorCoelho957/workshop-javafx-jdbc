7a236698e68d3ec24dc1edbe115194992b6f94ee
Perkles <victor.coelho@discente.ufma.br>
2023-04-29 20:52:18 -0300

    Department Service

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,9 +1,12 @@
 package gui;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -12,8 +15,11 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentListController implements Initializable {
+	private DepartmentService service; //declaração do metodo departmentservicer
+	
 	@FXML
 	private TableView<Department> tableViewDepartment;
 	
@@ -27,10 +33,21 @@ public class DepartmentListController implements Initializable {
 	@FXML
 	private Button btNew;
 	
+	//variavel para carrehar os departamentos
+	//@FXML
+	private ObservableList<Department> obsList;
+	
+	
 	//eventos do botão
 	public void onBtNewAction() {
 		System.out.println("onBtNewAction");
 	}
+	
+	//metodo set para ter dependencia do department service
+	public void setDepartmentService(DepartmentService service) {
+		this.service=service;
+	}
+	
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		//inixiar componente nas tabelas
@@ -48,6 +65,15 @@ public class DepartmentListController implements Initializable {
 		tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
 
 		
+	}
+	//metodo para carregar os departamentos e passar para obslist
+	public void updateTableView() {
+		if(service==null) {
+			throw new IllegalStateException("Service was null");	
+		}
+		List<Department> list=service.findAll();
+		obsList= FXCollections.observableArrayList(list);
+		tableViewDepartment.setItems(obsList);
 	}
 	
 
