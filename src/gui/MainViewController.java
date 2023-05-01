77e5d73ee983d4b6e06fd62b1b753f4229ff1c4e
Perkles <victor.coelho@discente.ufma.br>
2023-05-01 11:21:54 -0300

    initializing action as parameter

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -3,6 +3,7 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 import application.Main;
 import gui.util.Alerts;
@@ -34,12 +35,16 @@ public class MainViewController implements Initializable {
 	//chamada dA DEPARTMENT LIST
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		loadView2("/gui/DepartmentList.fxml");
+		//função para inicilizar o novo comando contido no departmentlist
+		loadView("/gui/DepartmentList.fxml",(DepartmentListController controller)-> { 
+			controller.setDepartmentService(new DepartmentService());
+			controller.updateTableView();});
 
 	}
 	@FXML
 	public void onMenuItemAboutAction() {
-		loadView("/gui/About.fxml");
+		//essa janela nao possui açãoa alguuma
+		loadView("/gui/About.fxml", (x->{}));
 	}
 	
 	
@@ -51,7 +56,7 @@ public class MainViewController implements Initializable {
 	}
 
 	//funcçao  para abir uma outra tela
-	private synchronized void loadView(String absoluteName) {
+	private synchronized <T >void loadView(String absoluteName, Consumer<T> initializingAction) {
 		try {
 			FXMLLoader loader= new FXMLLoader(getClass().getResource(absoluteName));
 			VBox newVBox=loader.load();
@@ -64,7 +69,9 @@ public class MainViewController implements Initializable {
 			mainVBox.getChildren().add(mainMenu);
 			//adiciona a lista
 			mainVBox.getChildren().addAll(newVBox.getChildren());
-			
+			//ativação ou iniacilação, passam qualquer metodo como argumneto de controller, seja departmentservice ou about
+			T controller=loader.getController();
+			initializingAction.accept(controller);
 		}
 		catch(IOException e) {
 			Alerts.showAlert("IO Exception", "Error loading", e.getMessage(), AlertType.ERROR);
@@ -72,28 +79,6 @@ public class MainViewController implements Initializable {
 		
 		}
 	
-	private synchronized void loadView2(String absoluteName) {
-		try {
-			FXMLLoader loader= new FXMLLoader(getClass().getResource(absoluteName));
-			VBox newVBox=loader.load();
-			//instancia da cena
-			Scene mainScene=Main.getMainScene();
-			VBox mainVBox=(VBox) ((ScrollPane) mainScene.getRoot()).getContent();
-			//exibir os filhos da Vbox
-			Node mainMenu= mainVBox.getChildren().get(0);
-			mainVBox.getChildren().clear();
-			mainVBox.getChildren().add(mainMenu);
-			//adiciona a lista
-			mainVBox.getChildren().addAll(newVBox.getChildren());
-			DepartmentListController controller =loader.getController();
-			controller.setDepartmentService(new DepartmentService());
-			controller.updateTableView();
-		}
-		catch(IOException e) {
-			Alerts.showAlert("IO Exception", "Error loading", e.getMessage(), AlertType.ERROR);
-		}
-		
-		}
 	
 	//chamada dA DEPARTMENT LIST
 	
