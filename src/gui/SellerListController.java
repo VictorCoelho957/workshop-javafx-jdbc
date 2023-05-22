5d255aee8a5015e78894a050f5931ce3193c9d07
Perkles <victor.coelho@discente.ufma.br>
2023-05-22 16:05:16 -0300

    TextField e DatePicker

--- src/gui/SellerListController.java
+++ src/gui/SellerListController.java
@@ -31,7 +31,6 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Seller;
-import model.entities.Seller;
 import model.services.SellerService;
 
 public class SellerListController implements Initializable, DataChangeListener {
